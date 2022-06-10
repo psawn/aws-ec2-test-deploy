acf1991d788e4021b79ba0535662fb078d092a31
Doan Duc Bao <bao.doan@savvycomsoftware.com>
2022-06-10 09:58:19 +0700

    fix application start

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
 sudo yum install -y nodejs
+node -v
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
