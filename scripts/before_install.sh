d7b476ec2c8226b8f25681c8da2cedd15697a7dd
Doan Duc Bao <bao.doan@savvycomsoftware.com>
2022-06-10 09:47:10 +0700

    fix script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
